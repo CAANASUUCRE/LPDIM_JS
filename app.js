8dd67af88bf8b6d2a9a7b298ccd4d3ca48d306b4
CAANASUUCRE <43761121+CAANASUUCRE@users.noreply.github.com>
2023-04-28 03:56:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var listener = app.listen(8888, function(){
+var listener = app.listen(3100, function(){
     console.log('Listening on port ' + listener.address().port);
 });
 app.use('/', indexRouter);
